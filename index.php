bf95e869c7c96a903609be76ec2ed344c0539db3
sarah <sarah.ahmed@freedomdev.com>
2021-04-10 10:21:56 +0300

    question 6

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
 <?php
 echo "Welcome!";
 ?>
+<html>
+<body>
+<h2>Automobile form</h2>
+    <form action="form1.php" method="POST">
+    <label for="licensePlate">Enter License Plate number: </label>
+    <input type="text" name="licensePlate">
+    <label for="brand">Enter brand: </label>
+    <input type="text" name="brand">
+    <label for="model">Enter model: </label>
+    <input type="text" name="model">
+    <label for="modelYear">Enter model year: </label>
+    <input type="text" name="modelYear">
+    <label for="color">Enter color: </label>
+    <input type="text" name="color">
+    </form>
+</body>
+</html>
+
